a74d39a93906f633da34377c7bd8ccf45abb2776
Dude112113 <curtisdude12@gmail.com>
2018-12-09 21:51:16 +0100

    GuideBookNoCrashy

--- src/main/java/com/imt/infinityscience/book/GuideBook.java
+++ src/main/java/com/imt/infinityscience/book/GuideBook.java
@@ -3,9 +3,9 @@ package com.imt.infinityscience.book;
 import com.imt.infinityscience.items.itemsubclasses.ItemBase;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
@@ -21,8 +21,7 @@ public class GuideBook extends ItemBase
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
 	{
 		ItemStack item = playerIn.getHeldItem(handIn);
-		return null;
-		
+		return new ActionResult(EnumActionResult.SUCCESS, null);
 	}
 
 }
