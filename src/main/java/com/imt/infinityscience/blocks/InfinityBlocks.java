921685ec6925e42f21af69626ac60b67b72d7430
MonolinkTV <30533416+MonolinkTV@users.noreply.github.com>
2018-12-18 19:28:45 -0500

    3 New Blocks and the new ConnectTextureBlock class

--- src/main/java/com/imt/infinityscience/blocks/InfinityBlocks.java
+++ src/main/java/com/imt/infinityscience/blocks/InfinityBlocks.java
@@ -3,6 +3,8 @@ package com.imt.infinityscience.blocks;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.imt.infinityscience.blocks.subclasses.ConnectTextureBlock;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
@@ -10,7 +12,16 @@ public class InfinityBlocks
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
+	
+	//Decorative Basic
 	public static final Block BLOCK_MARBLE =  new BlockBase("block_marble", Material.GROUND);
+	public static final Block BLOCK_BASALT = new BlockBase("block_basalt", Material.GROUND);
+	
+	//Decorative Tiled
+	public static final Block BASALT_TILED = new ConnectTextureBlock("basalt_tiled", Material.GROUND);
+	public static final Block MARBLE_TILED = new ConnectTextureBlock("marble_tiled", Material.GROUND);
+
+	
 	
 	//Ores
 	public static final Block BLOCK_COPPERORE =  new BlockBase("block_copperore", Material.ROCK);
---
+++ src/main/java/com/imt/infinityscience/blocks/subclasses/ConnectTextureBlock.java
@@ -0,0 +1,23 @@
+package com.imt.infinityscience.blocks.subclasses;
+
+import com.imt.infinityscience.blocks.BlockBase;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public class ConnectTextureBlock extends BlockBase
+{
+
+	public ConnectTextureBlock(String name, Material material) 
+	{
+		super(name, material);
+		
+		 
+	}
+}
